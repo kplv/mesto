407ac189af3331d983a6c769629897b7e6c02dea
Denis Kopylov <denuc.kopylov@gmail.com>
2020-08-10 23:06:23 +0300

    bigfix: поправить комментарии после ревью

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,17 @@ let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = popup.querySelector('.popup__close-button');
 let submitButton = popup.querySelector('.popup__submit-button');
-let formElement = document.querySelector('.popup__container');
+
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_name'); // Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector('.popup__input_job'); // Воспользуйтесь инструментом .querySelector()
+
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+
+let name = nameInput.value;
+let job = jobInput.value;
+
 
 function statePopup (event) {
 
@@ -12,19 +22,10 @@ function statePopup (event) {
       popup.classList.add('popup_opened')
     }
 
-    let nameInput = formElement.querySelector('.popup__input_name'); // Воспользуйтесь инструментом .querySelector()
-    let jobInput = formElement.querySelector('.popup__input_job'); // Воспользуйтесь инструментом .querySelector()
-
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__job');
-
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 
   }
-
-  editButton.addEventListener('click', statePopup);
-  closeButton.addEventListener('click', statePopup);
 
   function sendToServer (event) {
     event.preventDefault();
@@ -33,21 +34,14 @@ function statePopup (event) {
 
   function saveInfo (event) {
 
-    let nameInput = formElement.querySelector('.popup__input_name'); // Воспользуйтесь инструментом .querySelector()
-    let jobInput = formElement.querySelector('.popup__input_job'); // Воспользуйтесь инструментом .querySelector()
+    name = nameInput.value;
+    job = jobInput.value;
 
-    // Получите значение полей из свойства value
-    let name = nameInput.value;
-    let job = jobInput.value;
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__job');
-
-    // Вставьте новые значения с помощью textContent
-      profileName.textContent = name;
-      profileJob.textContent = job;
+    profileName.textContent = name;
+    profileJob.textContent = job;
   }
 
   submitButton.addEventListener('click', saveInfo);
   formElement.addEventListener('submit', sendToServer);
+  editButton.addEventListener('click', statePopup);
+  closeButton.addEventListener('click', statePopup);
