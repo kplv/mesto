466ef5c694808130d4403d7c1a55dbea5d080160
Denis Kopylov <denuc.kopylov@gmail.com>
2020-10-04 19:25:26 +0300

    web is ready

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import {items} from '../utils/constants.js';
-import Card from '../components/Card.js';
-import FormValidator from '../components/FormValidator.js';
-import Section from '../components/Section.js';
-import PopupWithImage from '../components/PopupWithImage.js';
-import PopupWithForm from '../components/PopupWithForm.js';
-import UserInfo from '../components/UserInfo.js';
+import {items} from './scripts/utils/constants.js';
+import Card from './scripts/components/Card.js';
+import FormValidator from './scripts/components/FormValidator.js';
+import Section from './scripts/components/Section.js';
+import PopupWithImage from './scripts/components/PopupWithImage.js';
+import PopupWithForm from './scripts/components/PopupWithForm.js';
+import UserInfo from './scripts/components/UserInfo.js';
+import './pages/index.css'
 
 const containerCards = document.querySelector('.elements');
 const cardListSelector = '.elements';
